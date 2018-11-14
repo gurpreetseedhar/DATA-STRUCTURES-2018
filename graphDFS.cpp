79a92e77ef7f652c90fee73e3f6ee98769628297
Gurpreet <32295205+gurpreetseedhar@users.noreply.github.com>
2018-11-15 00:16:06 +0530

    dfs(graphs)

---
+++ graphDFS.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int cost[10][10],visited[10],visit[10];
+int main()
+{
+    int n,e,v1,v2,v;
+    cout<<"enter no. of vertexes: ";
+    cin>>n;
+    cout<<"enter no. of edges: ";
+    cin>>e;
+    for(int k=0;k<e;k++)
+    {
+        cin>>v1>>v2;
+        cost[v1][v2]=1;
+    }
+    cout<<"enter initial vertex: ";
+    cin>>v;
+    cout<<v<<endl;
+    visited[v]=1;
+    int k=1;
+    stack <int> stk;
+    while(k<n)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(cost[v][j]!=0&&visited[j]!=1&&visit[j]!=1)
+            {
+                visit[j]=1;
+                stk.push(j);
+            }
+        }
+        v=stk.top();
+        stk.pop();
+        cout<<v<<endl;
+        k++;
+        visit[v]=0;
+        visited[v]=1;
+    }
+    return 0;
+}
